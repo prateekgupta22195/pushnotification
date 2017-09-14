b9ca662b828822526c0a05a6d93ec6972d02e2af
prateekgupta22195 <prateekgupta22195@gmail.com>
2017-09-14 22:48:21 +0530

    push noti done

---
+++ app/src/main/java/com/root/firebasepractice/Messages.java
@@ -0,0 +1,17 @@
+package com.root.firebasepractice;
+
+/**
+ * Created by root on 14/9/17.
+ */
+
+public class Messages {
+    private String MESSAGE = "";
+
+    public String getMESSAGE() {
+        return MESSAGE;
+    }
+
+    public void setMESSAGE(String MESSAGE) {
+        this.MESSAGE = MESSAGE;
+    }
+}
